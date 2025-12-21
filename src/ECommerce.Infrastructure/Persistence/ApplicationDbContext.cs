2f87fbbaa968b80b5e1d477657e882586191eb7e
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-22 00:12:16 +0400

    Add Product and Category DbSets to ApplicationDbContext
    Refactored ApplicationDbContext to use a strongly-typed constructor and added DbSet properties for Product and Category entities. Overrode OnModelCreating to apply configurations from the assembly. Updated using directives and improved EF Core setup best practices.

--- src/ECommerce.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/ECommerce.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECommerce.Domain.Products.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace ECommerce.Infrastructure.Persistence;
+
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions options) : base(options)
-    {        
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 }
