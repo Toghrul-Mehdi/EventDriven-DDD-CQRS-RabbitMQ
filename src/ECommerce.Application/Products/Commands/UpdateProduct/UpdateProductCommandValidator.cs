b5c0cb5b9f086a05ba36dd6395a82b09d8b80417
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-22 23:42:06 +0400

    feat: Add UpdateProductCommand validator - Validated price, stock and category existence

---
+++ src/ECommerce.Application/Products/Commands/UpdateProduct/UpdateProductCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+namespace ECommerce.Application.Products.Commands.UpdateProduct;
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required")
+            .MaximumLength(200).WithMessage("Product name must not exceed 200 characters");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than zero");
+
+        RuleFor(x => x.Stock)
+            .GreaterThanOrEqualTo(0).WithMessage("Stock cannot be negative");
+
+        RuleFor(x => x.CategoryId)
+            .NotEmpty().WithMessage("Category is required");
+    }
+}
