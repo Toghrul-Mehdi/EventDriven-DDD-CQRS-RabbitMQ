421c9e30cd5ce815b06d473316cee656e9cfa7cb
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-22 23:41:24 +0400

    feat: Add CreateProductCommand validator - Added FluentValidation rules for title, price, stock, etc.

---
+++ src/ECommerce.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace ECommerce.Application.Products.Commands.CreateProduct;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Product name is required")
+            .MaximumLength(200).WithMessage("Product name must not exceed 200 characters");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than zero");
+
+        RuleFor(x => x.Stock)
+            .GreaterThanOrEqualTo(0).WithMessage("Stock cannot be negative");
+
+        RuleFor(x => x.CategoryId)
+            .NotEmpty().WithMessage("Category is required");
+    }
+}
