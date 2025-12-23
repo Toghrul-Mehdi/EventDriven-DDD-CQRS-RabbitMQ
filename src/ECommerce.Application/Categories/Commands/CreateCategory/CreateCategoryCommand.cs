100b31f68e50313cb635fd8161e5dfd150474d05
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-23 23:55:02 +0400

    feat: Add CreateCategoryCommand and handler - Implemented category creation with name and slug generation

---
+++ src/ECommerce.Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using ECommerce.SharedKernel.Domain;
+using MediatR;
+namespace ECommerce.Application.Categories.Commands.CreateCategory;
+public record CreateCategoryCommand(
+    string Name,
+    string Description
+    ) : IRequest<Result<string>>;
+
