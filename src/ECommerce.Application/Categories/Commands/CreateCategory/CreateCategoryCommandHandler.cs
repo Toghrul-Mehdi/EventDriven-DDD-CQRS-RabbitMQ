cdf14b473be55b1245e60d2bd5d5b8890f9ee202
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-23 23:55:19 +0400

    feat: Add CreateCategoryCommand DTOs - Created request and response models for category creation

---
+++ src/ECommerce.Application/Categories/Commands/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.Domain.Products.Entities;
+using ECommerce.Domain.Products.Repositories;
+using ECommerce.SharedKernel.Domain;
+using MediatR;
+namespace ECommerce.Application.Categories.Commands.CreateCategory;
+
+public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Result<string>>
+{
+    private readonly ICategoryRepository _categoryRepository;
+    public CreateCategoryCommandHandler(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+    public async Task<Result<string>> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var exsistCategory = await _categoryRepository.ExistsAsync(request.Name,cancellationToken);
+        if (exsistCategory)
+        {
+            return Result<string>.Failure("Category name already exsist.");
+        }
+
+        var category = Category.Create(
+            request.Name,
+            request.Description
+        );
+
+        await _categoryRepository.AddAsync(category,cancellationToken);
+        await _categoryRepository.SaveChangesAsync(cancellationToken);
+
+        return Result<string>.Success(category.Id);
+    }
+}
