0d16990a1f219e53e8bbfb881a1788bab6693476
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-23 23:55:29 +0400

    feat: Add CreateCategoryCommand validator - Added validation for unique name and required fields

---
+++ src/ECommerce.Application/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+namespace ECommerce.Application.Categories.Commands.CreateCategory;
+public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+{
+    public CreateCategoryCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Category name is required")
+            .MaximumLength(200).WithMessage("Category name must not exceed 200 characters");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters");
+    }
+}
+ 
