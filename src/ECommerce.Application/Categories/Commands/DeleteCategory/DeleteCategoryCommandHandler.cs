3bf873ee81319a5589c1c65266156436cbe59414
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-23 23:55:45 +0400

    feat: Add DeleteCategoryCommand DTOs - Created simple request model with category Id

---
+++ src/ECommerce.Application/Categories/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.Domain.Products.Repositories;
+using ECommerce.SharedKernel.Domain;
+using MediatR;
+namespace ECommerce.Application.Categories.Commands.DeleteCategory;
+
+public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, Result<string>>
+{
+    private readonly ICategoryRepository _categoryRepository;
+    public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+    public async Task<Result<string>> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await _categoryRepository.GetByIdAsync(request.CategoryId, cancellationToken);
+        if (category is null)
+        {
+            return Result<string>.Failure("Category not found.");
+        }
+        category.Delete();
+        _categoryRepository.Delete(category);
+        await _categoryRepository.SaveChangesAsync(cancellationToken);
+        return Result<string>.Success("Category delete succesfully.");
+    }
+}
