0df47f359b30fee10e2897c3e545879159e2ed4d
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-24 23:23:30 +0400

    Add validation rules for UpdateCategory command
    Ensured category name constraints are enforced
    Improved input safety at application layer

--- src/ECommerce.Application/Categories/Commands/UpdateCategory/UpdateCategoryCommandValidator.cs
+++ src/ECommerce.Application/Categories/Commands/UpdateCategory/UpdateCategoryCommandValidator.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
-namespace ECommerce.Application.Categories.Commands.UpdateCategory
+namespace ECommerce.Application.Categories.Commands.UpdateCategory;
+
+public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
 {
-    internal class UpdateCategoryCommandValidator
+    public UpdateCategoryCommandValidator()
     {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Category Id is required");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Category name is required")
+            .MaximumLength(200).WithMessage("Category name must not exceed 200 characters");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000).WithMessage("Description must not exceed 1000 characters");
     }
 }
