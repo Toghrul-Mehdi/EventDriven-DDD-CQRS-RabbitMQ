5274d58ade634484b1f4f6f3f2186baf4ec7cbf4
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-24 23:24:41 +0400

    Add ProductCreated domain event
    Introduced event to represent product creation
    Prepared integration with messaging infrastructure

---
+++ src/ECommerce.Domain/Products/Events/ProductCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.SharedKernel.Domain;
+namespace ECommerce.Domain.Products.Events;
+public record ProductCreatedEvent(
+    string ProductId,
+    string ProductName,
+    string Description,
+    decimal Price,
+    int Stock,
+    string CategoryId
+) : DomainEvent;
