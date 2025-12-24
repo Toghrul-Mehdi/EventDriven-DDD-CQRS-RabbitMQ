f4d88913a3d1f2cfad1f381eec9bd0273c3e1902
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-24 23:36:01 +0400

    feat(infrastructure): add MediatorExtension for domain events - Implement domain event dispatcher - Retrieve entities with pending events - Publish events through MediatR - Clear events after dispatch

---
+++ src/ECommerce.Infrastructure/Extensions/MediatorExtension.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.SharedKernel.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Extensions;
+
+public static class MediatorExtension
+{
+    public static async Task DispatchDomainEventsAsync(this IMediator mediator, DbContext context)
+    {
+        var domainEntities = context.ChangeTracker
+            .Entries<Entity>()
+            .Where(x => x.Entity.DomainEvents != null && x.Entity.DomainEvents.Any())
+            .ToList();
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToList();
+
+        domainEntities.ForEach(entity => entity.Entity.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await mediator.Publish(domainEvent);
+        }
+    }
+}
