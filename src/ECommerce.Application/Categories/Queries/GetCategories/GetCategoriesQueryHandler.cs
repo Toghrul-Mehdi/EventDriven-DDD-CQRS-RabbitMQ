a9041d9abe5b4232ca2febf14dbfa853406fedca
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-24 23:23:49 +0400

    Improve GetCategories query handler
    Optimized data retrieval logic
    Ensured proper mapping to Category DTOs

--- src/ECommerce.Application/Categories/Queries/GetCategories/GetCategoriesQueryHandler.cs
+++ src/ECommerce.Application/Categories/Queries/GetCategories/GetCategoriesQueryHandler.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ECommerce.Domain.Products.Repositories;
+using ECommerce.SharedKernel.Domain;
+using MediatR;
 
-namespace ECommerce.Application.Categories.Queries.GetCategories
+namespace ECommerce.Application.Categories.Queries.GetCategories;
+
+public class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, Result<List<CategoryDto>>>
 {
-    internal class GetCategoriesQueryHandler
+    private readonly ICategoryRepository _categoryRepository;
+    public GetCategoriesQueryHandler(ICategoryRepository categoryRepository)
     {
+        _categoryRepository = categoryRepository;
+    }
+    public async Task<Result<List<CategoryDto>>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        var categories = await _categoryRepository.GetAllAsync(cancellationToken);
+
+        var categoryDtos = categories.Select(x => new CategoryDto
+        {
+            CategoryId = x.Id,
+            Name = x.Name,
+            Description = x.Description
+        }).ToList();
+
+        return Result<List<CategoryDto>>.Success(categoryDtos);       
     }
 }
