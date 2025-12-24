b6f660cb3e1655ec79917b9049e007500266a637
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-24 23:25:06 +0400

    Introduce base DomainEvent abstraction
    Centralized OccurredOnUtc handling
    Standardized domain event inheritance

---
+++ src/ECommerce.SharedKernel/Domain/Events/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.SharedKernel.Domain;
+public abstract record DomainEvent : IDomainEvent
+{
+    public DateTime OccurredOn { get; init; }
+    public Guid EventId { get; init; }
+
+    protected DomainEvent()
+    {
+        OccurredOn = DateTime.UtcNow;
+        EventId = Guid.NewGuid();
+    }
+}
