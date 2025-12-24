5dc7c45b721b8897bd32bc15d3d29275a6f62006
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-24 23:25:00 +0400

    Enhance base Entity abstraction
    Added domain event collection handling
    Prepared entities for event dispatching

--- src/ECommerce.SharedKernel/Domain/Entity.cs
+++ src/ECommerce.SharedKernel/Domain/Entity.cs
@@ -1,55 +1,73 @@
 ﻿namespace ECommerce.SharedKernel.Domain;
+
 public abstract class Entity
-{    
+{
     public string Id { get; protected set; }
-    
+
+    private List<IDomainEvent> _domainEvents;
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents?.AsReadOnly();
+
     protected Entity()
     {
         Id = Guid.NewGuid().ToString();
+        _domainEvents = new List<IDomainEvent>();
     }
-    
+
     protected Entity(string id)
     {
         Id = id;
+        _domainEvents = new List<IDomainEvent>();
     }
 
-    
+    public void AddDomainEvent(IDomainEvent eventItem)
+    {
+        _domainEvents = _domainEvents ?? new List<IDomainEvent>();
+        _domainEvents.Add(eventItem);
+    }
+
+    public void RemoveDomainEvent(IDomainEvent eventItem)
+    {
+        _domainEvents?.Remove(eventItem);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents?.Clear();
+    }
+
     public override bool Equals(object obj)
     {
         if (obj is not Entity other)
+        {
             return false;
-
+        }
         if (ReferenceEquals(this, other))
+        {
             return true;
-
+        }
         if (GetType() != other.GetType())
+        {
             return false;
-
+        }
         return Id == other.Id;
     }
 
-    
     public static bool operator ==(Entity a, Entity b)
     {
         if (a is null && b is null)
             return true;
-
         if (a is null || b is null)
             return false;
-
         return a.Equals(b);
     }
 
-    
     public static bool operator !=(Entity a, Entity b)
     {
         return !(a == b);
     }
 
-    
     public override int GetHashCode()
     {
         return Id.GetHashCode();
     }
 }
-
