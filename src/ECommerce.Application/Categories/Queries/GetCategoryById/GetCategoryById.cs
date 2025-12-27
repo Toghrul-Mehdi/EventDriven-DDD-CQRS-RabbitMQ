54ef1f1337edcaa0a17f7fbc6ffb6ec984c2b807
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-28 00:25:32 +0400

    feat(application): add get category by id query
    - Define query contract for retrieving category by id
    - Return category data with Result pattern

---
+++ src/ECommerce.Application/Categories/Queries/GetCategoryById/GetCategoryById.cs
@@ -0,0 +1,8 @@
+﻿using ECommerce.Application.Categories.Queries.GetCategories;
+using ECommerce.SharedKernel.Domain;
+using MediatR;
+
+namespace ECommerce.Application.Categories.Queries.GetCategoryById;
+public record GetCategoryById (string Id) : IRequest<Result<CategoryDto>>;
+
+
