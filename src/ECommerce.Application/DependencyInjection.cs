8fff7c9c3c9558cee8f158c859038aa777591a3d
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-28 00:25:45 +0400

    chore(application): configure application layer dependency injection
    - Register MediatR handlers
    - Add application service registrations

---
+++ src/ECommerce.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Application.Products.EventHandlers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(typeof(ProductCreatedEventHandler).Assembly));
+
+        return services;
+    }
+}
