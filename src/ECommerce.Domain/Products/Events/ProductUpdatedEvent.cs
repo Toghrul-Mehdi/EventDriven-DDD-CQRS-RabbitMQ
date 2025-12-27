b7fe4fc6dffbd403773e473f535a641015338c2e
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-27 23:55:45 +0400

    feat(domain): add ProductUpdatedEvent - Define event with updated product information - Track product modifications - Include ID, name and description

---
+++ src/ECommerce.Domain/Products/Events/ProductUpdatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.SharedKernel.Domain;
+
+namespace ECommerce.Domain.Products.Events;
+
+public record ProductUpdatedEvent(
+    string ProductId,
+    string ProductName,
+    string Description
+) : DomainEvent;
