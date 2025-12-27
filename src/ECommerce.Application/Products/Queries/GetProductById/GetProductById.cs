a212ffb5ed5af1bf529f17ecf5036b2935e76884
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-27 23:54:08 +0400

    feat(application): add GetProductById query - Implement query with Result pattern - Accept product ID as parameter - Return ProductDto with result wrapper

---
+++ src/ECommerce.Application/Products/Queries/GetProductById/GetProductById.cs
@@ -0,0 +1,7 @@
+﻿using ECommerce.Application.Products.Queries.GetProducts;
+using ECommerce.SharedKernel.Domain;
+using MediatR;
+
+namespace ECommerce.Application.Products.Queries.GetProductById;
+
+public record GetProductByIdQuery(string Id) : IRequest<Result<ProductDto>>;
