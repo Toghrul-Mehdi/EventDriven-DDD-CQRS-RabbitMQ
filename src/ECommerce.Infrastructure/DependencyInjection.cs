583f265083cd797af956be39dc941c6771dda198
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-27 23:56:44 +0400

    feat(application): add dependency injection configuration - Register MediatR with assembly scanning - Add FluentValidation validators from assembly - Configure CQRS infrastructure - Setup application services extension method

---
+++ src/ECommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Infrastructure.Persistence;
+using ECommerce.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SQlServer")));
+
+        services.Scan(scan => scan
+            .FromAssemblyOf<ProductRepository>()
+            .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Repository")))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
+}
