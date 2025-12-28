20537084ff48eed88d3ade22000cb8c47df103c6
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-28 23:55:54 +0400

    feat(application): add DeleteProductsByCategoryCommand with Result - Bulk operation returning Result<Unit> - Handled cascading delete safely

---
+++ src/ECommerce.Application/Products/Commands/DeleteProductsByCategory/DeleteProductsByCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using ECommerce.SharedKernel.Domain;
+using MediatR;
+
+namespace ECommerce.Application.Products.Commands.DeleteProductsByCategory;
+
+public  record DeleteProductsByCategoryCommand(string CategoryId)
+    : IRequest<Result>;
+
