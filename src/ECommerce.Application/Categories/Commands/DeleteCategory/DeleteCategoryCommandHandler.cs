aa76e8f584d607504ad274631d2c119a0a46cd8b
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-28 23:54:20 +0400

    feat(application): add DeleteCategoryCommand using Result - Returned Result<Unit> on successful deletion - Handled "not found" and "has children" failures

--- src/ECommerce.Application/Categories/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
+++ src/ECommerce.Application/Categories/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -2,23 +2,31 @@
 using ECommerce.SharedKernel.Domain;
 using MediatR;
 namespace ECommerce.Application.Categories.Commands.DeleteCategory;
-public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, Result<string>>
+public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, Result>
 {
     private readonly ICategoryRepository _categoryRepository;
     public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository)
     {
         _categoryRepository = categoryRepository;
     }
-    public async Task<Result<string>> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    public async Task<Result> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
     {
-        var category = await _categoryRepository.GetByIdAsync(request.CategoryId, cancellationToken);
-        if (category is null)
+        var category = await _categoryRepository.GetWithProductsAsync(request.CategoryId, cancellationToken);
+
+        if (category == null)
         {
-            return Result<string>.Failure("Category not found.");
+            return Result.NotFound("Category not found");
         }
+
+        if (category.HasProducts())
+        {
+            return Result.Conflict("Cannot delete category with existing products");
+        }
+
         category.Delete();
         _categoryRepository.Delete(category);
         await _categoryRepository.SaveChangesAsync(cancellationToken);
-        return Result<string>.Success("Category delete succesfully.");
+
+        return Result.NoContent("Category deleted successfully");
     }
 }
