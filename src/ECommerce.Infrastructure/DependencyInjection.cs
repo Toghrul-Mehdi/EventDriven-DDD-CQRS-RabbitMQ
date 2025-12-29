f5ffcca3db79f709ff73071a9ccb224872a81a8c
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-29 23:05:47 +0400

    feat(infrastructure): register RabbitMQ settings in dependency injection
    - Added Configure<RabbitMQSettings> using configuration section
    - Enables strongly-typed options pattern for RabbitMQ connection

--- src/ECommerce.Infrastructure/DependencyInjection.cs
+++ src/ECommerce.Infrastructure/DependencyInjection.cs
@@ -1,23 +1,29 @@
-﻿using ECommerce.Infrastructure.Persistence;
+﻿using ECommerce.Infrastructure.MessageBroker.RabbitMQ;
+using ECommerce.Infrastructure.Persistence;
 using ECommerce.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ECommerce.Infrastructure;
+
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("SQlServer")));
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("SQlServer")));
 
         services.Scan(scan => scan
             .FromAssemblyOf<ProductRepository>()
             .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Repository")))
             .AsImplementedInterfaces()
             .WithScopedLifetime());
+
+        services.Configure<RabbitMQSettings>(configuration.GetSection("RabbitMQ"));
+
 
         return services;
     }
