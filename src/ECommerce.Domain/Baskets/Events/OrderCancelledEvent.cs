148d6a4a6629a0e4c181815e4b5730968089ced6
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2025-12-31 22:44:52 +0400

    feat(domain): add OrderCanceled domain event - Introduced domain event for order cancellation - Enables event-driven reactions on cancel action - Improves decoupling between domain and application layers

---
+++ src/ECommerce.Domain/Baskets/Events/OrderCancelledEvent.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.SharedKernel.Domain;
+
+namespace ECommerce.Domain.Baskets.Events;
+
+public record OrderCancelledEvent(
+    string OrderId,
+    string UserId,
+    decimal TotalAmount
+) : DomainEvent;
