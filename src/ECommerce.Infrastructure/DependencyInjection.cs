fa8e6d70dfde1ecd68d6d9e720f550d832ca4035
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2026-01-02 21:01:46 +0400

    refactor(infrastructure): enhance service registration extensions
    - Improved DependencyInjection extension methods for better modularity
    - Added conditional service registrations based on configuration
    - Cleaned up duplicate or obsolete registrations

--- src/ECommerce.Infrastructure/DependencyInjection.cs
+++ src/ECommerce.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,10 @@
-﻿using ECommerce.Infrastructure.MessageBroker.RabbitMQ;
+﻿using ECommerce.Application.Common.Interfaces;
+using ECommerce.Domain.Users.Entities;
+using ECommerce.Infrastructure.MessageBroker.RabbitMQ;
 using ECommerce.Infrastructure.Persistence;
 using ECommerce.Infrastructure.Persistence.Repositories;
+using ECommerce.Infrastructure.Persistence.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,11 +20,18 @@ public static class DependencyInjection
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("SQlServer")));
 
+        services.AddIdentity<AppIdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
         services.Scan(scan => scan
             .FromAssemblyOf<ProductRepository>()
             .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Repository")))
             .AsImplementedInterfaces()
             .WithScopedLifetime());
+
+
+        services.AddScoped<IAuthService, AuthService>();
 
         services.Configure<RabbitMQSettings>(configuration.GetSection("RabbitMQ"));
 
