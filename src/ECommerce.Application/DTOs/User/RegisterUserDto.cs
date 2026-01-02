cc9d8b77b748401e953241aeff924505b8509c06
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2026-01-02 21:03:54 +0400

    feat(auth): add DTO for user registration
    - Introduced RegisterUserDto with required fields (Email, Password, ConfirmPassword, FullName etc.)
    - Designed for use in registration command and API endpoints

---
+++ src/ECommerce.Application/DTOs/User/RegisterUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Application.DTOs.User;
+public class RegisterUserDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public DateTime DateBirth { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+    public bool AcceptTerms { get; set; }
+}
