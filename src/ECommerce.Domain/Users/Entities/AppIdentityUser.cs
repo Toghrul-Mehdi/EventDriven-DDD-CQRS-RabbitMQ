da7dbf46b4619fef100e69a3f11a39e16b12e702
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2026-01-02 21:05:53 +0400

    feat(identity): create custom identity user class
    - Introduced AppIdentityUser inheriting from IdentityUser
    - Added custom properties (FullName, RefreshToken etc.)
    - Serves as base user class for entire application

---
+++ src/ECommerce.Domain/Users/Entities/AppIdentityUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ECommerce.Domain.Users.Entities;
+
+public class AppIdentityUser : IdentityUser
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime DateBirth { get; set; }
+}
