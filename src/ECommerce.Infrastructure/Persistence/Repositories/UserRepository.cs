24dbef4264bad4e13be3fc2d36c61689d8a16ea6
Toghrul-Mehdi <togrul.mehdiyev05@gmail.com>
2026-01-02 21:02:50 +0400

    feat(infrastructure): implement concrete UserRepository for persistence
    - Created UserRepository implementing IUserRepository interface
    - Added CRUD operations using EF Core
    - Included query optimizations and async support

---
+++ src/ECommerce.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Domain.Users.Entities;
+using ECommerce.Domain.Users.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly UserManager<AppIdentityUser> _userManager;
+
+    public UserRepository(UserManager<AppIdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<List<AppIdentityUser>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _userManager.Users.ToListAsync();
+    }
+
+    public async Task<AppIdentityUser?> GetByEmailAsync(string email,CancellationToken cancellationToken)
+    {
+        return await _userManager.FindByEmailAsync(email);
+    }
+
+    public async Task<AppIdentityUser?> GetByIdAsync(string id,CancellationToken cancellationToken)
+    {
+        return await _userManager.FindByIdAsync(id);
+    }
+}
